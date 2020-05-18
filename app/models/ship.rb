92762e7a68f5e6ce3f13bf31db88f26b4f313477
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-18 12:38:48 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  
+  attr_reader :name, :type, :booty
+  
+  @@all = []
+  
+  def initialize(name, type, booty)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear
+    @@all.clear
+  end
 end
